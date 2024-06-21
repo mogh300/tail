c98ca877e9023f3523c101e6e93fa7f7fbc8d78d
mogh300 <mogh300@gmail.com>
2024-06-21 19:39:39 +0330

    kodamin

--- src/components/Mid.jsx
+++ src/components/Mid.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import YellowButton from './YellowButton'
-
 const Mid = () => {
   return (
-   
-
-    <section className=' relative  mt-28  container   '>
-      <img className='lg:absolute max-md:hidden h-lvh  -z-10  ' src=".\img\Ellipse 1328.png" />  
-        <h1 className=' text-4xl font-bold  text-center'>About me</h1>
+    <section className=' relative  mt-28  container'>
+      <img className='lg:absolute max-md:hidden h-lvh -z-10' src=".\img\Ellipse 1328.png" />  
+        <h1 className=' text-4xl font-bold text-center'>About me</h1>
         <p className='  text-InkScribe my-5 text-2xl text-center' >Designing solutions, not just a visuals!</p>
-    
-      <div className=' flex flex-col lg:flex-row  my-9  text-center md:text-left z-20  '>
-       
+      <div className=' flex flex-col lg:flex-row my-9  text-center md:text-left z-20'>
       <div>
        <div className='my-10'>
                 <button className=' mt-20 rounded-2xl w-10  bg-rose-400   text-white' >01</button>
@@ -24,15 +19,10 @@ const Mid = () => {
                 <p className=' pb-4 text-InkScribe ' >Stacks is a production-ready library of stackable content blocks built in React Native</p>
                 <YellowButton>Download CV👉</YellowButton>
               </div>
-    
          </div>
-         <img src="./img/about illustration.png" alt="" />
+         <img src="./img/about illustration.png"/>
       </div>
-  
-  
     </section>
- 
   )
 }
-
 export default Mid
