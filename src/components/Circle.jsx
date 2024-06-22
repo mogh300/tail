8644f9cc777b2d7d72a532608f1d97cc3c2f3ce7
mogh300 <mogh300@gmail.com>
2024-06-22 10:11:30 +0330

    finalist

---
+++ src/components/Circle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Circle = () => {
+  return (
+    <img className='lg:absolute max-lg:hidden max-h-screen -z-10' src=".\img\Ellipse 1328.png" />  
+  )
+}
+
+export default Circle
--- src/components/Mid.jsx
+++ src/components/Mid.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import YellowButton from './YellowButton'
 const Mid = () => {
   return (
-    <section className=' relative  mt-28  container'>
-      <img className='lg:absolute max-lg:hidden h-lvh -z-10' src=".\img\Ellipse 1328.png" />  
+    <section className=' relative  mt-28  z-0 container' >
+      <img className='lg:absolute -start-10 max-lg:hidden max-h-screen -z-10' src=".\img\Ellipse 1328.png" />  
         <h1 className=' text-4xl font-bold text-center'>About me</h1>
         <p className='  text-InkScribe my-5 text-2xl text-center' >Designing solutions, not just a visuals!</p>
       <div className=' flex flex-col lg:flex-row my-9  text-center md:text-left z-20'>
       <div>
-       <div className='my-10'>
+       <div className='my-10 z-0'>
                 <button className=' mt-20 rounded-2xl w-10  bg-rose-400   text-white' >01</button>
                 <h2 className=' py-4 text-2xl '>1K+ complete project</h2>
                 <p className=' text-InkScribe' >Stacks is a production-ready library of stackable content blocks built in React Native</p>
